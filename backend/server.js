0b3d31f2dd4426fed353e1910a7982277c991957
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-04-16 14:22:47 -0600

    Add Helmet middlewate for security

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
 
 const app = express();
 
+app.use(helmet());
+// Helmet is a security middleware that helps you protect your Express apps by setting various HTTP headers
+
 app.get("/", (req, res) => {
+  console.log(res.getHeaders());
   res.send("Hello from the backend!");
 });
 
