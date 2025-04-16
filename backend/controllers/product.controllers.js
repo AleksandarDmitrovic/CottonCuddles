967d986d37ce1c4a850d6f1f6c4f20e04eb122b1
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-04-16 15:52:21 -0600

    Create product controller & get and post api calls for products

---
+++ backend/controllers/product.controllers.js
@@ -0,0 +1,7 @@
+export const getProducts = async (req, res) => {
+  // GET ALL PRODUCTS
+  res.send("Product route is working");
+}
+
+export const createProduct = async (req, res) => {
+};
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import {
+  createProduct,
+  getProducts,
+} from "../controllers/product.controllers.js";
 
 const router = express.Router();
+
+router.get("/", getProducts);
+router.post("/", createProduct);
 
 export default router;
