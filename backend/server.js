08ea6dffe88cc6e71ecd3c6cc73487984fb31757
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-04-16 15:04:14 -0600

    Use PORT var for

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 console.log("PORT :", PORT);
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 // Helmet is a security middleware that helps you protect your Express apps by setting various HTTP headers
-app.use(morgan("dev")); 
+app.use(morgan("dev"));
 // Log the requests to the console
 
 app.get("/", (req, res) => {
@@ -22,6 +22,6 @@ app.get("/", (req, res) => {
   res.send("Hello from the backend!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
