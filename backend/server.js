564291fc8cc90c866aaf8b67a11450e0cdcf481e
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-04-16 14:29:58 -0600

    Add server PORT variable

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
+console.log("PORT :", PORT);
 
 app.use(express.json());
 app.use(cors());
