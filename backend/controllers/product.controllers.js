94be0c233af0ca269cb54e29f9d8a157c018102d
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-04-18 17:01:48 -0600

    Initialize all product routes

--- backend/controllers/product.controllers.js
+++ backend/controllers/product.controllers.js
@@ -1,7 +1,9 @@
 export const getProducts = async (req, res) => {
   // GET ALL PRODUCTS
   res.send("Product route is working");
-}
-
-export const createProduct = async (req, res) => {
 };
+
+export const createProduct = async (req, res) => {};
+export const getProduct = async (req, res) => {};
+export const updateProduct = async (req, res) => {};
+export const deleteProduct = async (req, res) => {};
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,12 +1,18 @@
-import express from 'express';
+import express from "express";
 import {
-  createProduct,
   getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
 } from "../controllers/product.controllers.js";
 
 const router = express.Router();
 
 router.get("/", getProducts);
+router.get("/:id", getProduct);
 router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
