0ad8ff528ece9d3383bad4140f96715491ed9baf
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-04-18 13:13:58 -0600

    Create a couple of test server routes

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import productRoutes from "./routes/product.routes.js";
+import pool from "./db/index.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-console.log("PORT :", PORT);
 
 app.use(express.json());
 app.use(cors());
@@ -19,6 +20,26 @@ app.use(morgan("dev"));
 // Log the requests to the console
 
 app.use("/api/products", productRoutes);
+
+app.get("/", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.post("/", (req, res) => {
+  const { name, location } = req.body;
+
+  res.status(200).send({
+    message: `Your Keys are ${name} and ${location}`,
+  });
+});
+
+// const initDB = async () => {
+//   // Initialize the database connection here
+//  try{
+//   await db
+//  }
+//   console.log("Database initialized");
+// }
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
