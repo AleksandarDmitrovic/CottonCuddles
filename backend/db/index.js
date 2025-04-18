a43e1ed7e531249978dc7f312e7778d33034ecbf
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-04-18 13:02:05 -0600

    Create client connection to pg database

---
+++ backend/db/index.js
@@ -0,0 +1,16 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { DB_HOST, DB_USER, DB_PORT, DB_PASSWORD, DB_NAME } = process.env;
+
+const client = new Pool({
+  host: DB_HOST,
+  user: DB_USER,
+  port: DB_PORT,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+});
+
+modules.exports = { client };
